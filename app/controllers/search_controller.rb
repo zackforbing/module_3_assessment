c10686b87de9b738fff796d94ecb699d74f9c15c
Zack Forbing <zackforbing@gmail.com>
2016-09-20 12:01:53 -0600

    working on building the request/response in the controller, request was parsing weird

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,10 @@
 class SearchController < ApplicationController
 
   def index
-    conn = Faraday.get("https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=tq72hpvtbbd4mue6f23kmsw2&show=longName,city,location,phone,storeType,distance&pageSize=15&callback=JSON_CALLBACK&format=json")
-    conn.params[:apiKey] = 'tq72hpvtbbd4mue6f23kmsw2'
-    require "pry"; binding.pry
+    request = "https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=tq72hpvtbbd4mue6f23kmsw2&show=longName,city,location,phone,storeType,distance&pageSize=15&callback=JSON_CALLBACK&format=json"
+    response = Faraday.get(request)
+    @stores = JSON.parse(response.body)['stores'].map do |store|
+      OpenStruct.new(store)
+    end
   end
 end
